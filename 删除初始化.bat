214a766973c3e888ee818214f74bb3edb0ec68eb
Allen <youthallen@163.com>
2019-03-21 15:50:33 +0800

    first init

---
+++ 删除初始化.bat
@@ -0,0 +1,14 @@
+D:
+cd D:\lab\fbs-shop
+
+rmdir /s/q .git
+
+git init
+
+git remote add ee-shop git@gitee.com:Freakboy/fbs-shop.git
+
+git remote add hub-fbs git@github.com:Freakboy/fbs-shop.git
+
+git remote -v
+
+pause
---
+++ 提交初始化.bat
@@ -0,0 +1,14 @@
+D:
+cd D:\lab\fbs-shop
+
+git status
+
+git add .
+
+git commit -am "first init"
+
+git push hub-fbs master -f
+
+git push ee-shop master -f
+
+pause
